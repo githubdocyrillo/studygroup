ca845751ff8548e0c19e07adab7a72eb6b396176
Cyrillo Heimart <cyrillo.heimart@12120-MB.local>
2023-01-18 11:15:59 -0300

    feat: Add useStateSample

--- pages/hooks/use-reducer.tsx
+++ pages/hooks/use-reducer.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import {
   Light,
   TrafficLight,
@@ -16,20 +16,20 @@ const UseReducerSample: NextPage = () => {
     TraficLightState.STOP
   );
 
-  const map = (current: TraficLightState) => (
-    <Light
-      key={current}
-      actived={current === actived}
-      color={current}
-      onClick={() => setActived(current)}
-    ></Light>
+  const map = useCallback(
+    (current: TraficLightState) => (
+      <Light
+        key={current}
+        actived={current === actived}
+        color={current}
+        onClick={() => setActived(current)}
+      ></Light>
+    ),
+    [actived]
   );
 
   return (
-    <TrafficLight>
-      <h1>GO</h1>
-      {Object.values(TraficLightState).map(map)}
-    </TrafficLight>
+    <TrafficLight>{Object.values(TraficLightState).map(map)}</TrafficLight>
   );
 };
 
--- pages/hooks/use-state.tsx
+++ pages/hooks/use-state.tsx
@@ -1,17 +1,37 @@
 import { NextPage } from "next";
-import React from "react";
+import React, { useCallback, useState } from "react";
 import {
   Light,
   TrafficLight,
 } from "../../components/TrafficLight/TrafficLight.styles";
 
+enum TrafficLightState {
+  STOP = "red",
+  SLOW_DOWN = "orange",
+  GO = "green",
+}
+
 const UseStateSample: NextPage = () => {
+  const [actived, setActived] = useState<TrafficLightState>(
+    TrafficLightState.STOP
+  );
+
+  const map = useCallback(
+    (value: TrafficLightState) => {
+      return (
+        <Light
+          key={value}
+          actived={actived === value}
+          color={value}
+          onClick={() => setActived(value)}
+        ></Light>
+      );
+    },
+    [actived]
+  );
+
   return (
-    <TrafficLight>
-      <Light color="red"></Light>
-      <Light color="yellow"></Light>
-      <Light color="green"></Light>
-    </TrafficLight>
+    <TrafficLight>{Object.values(TrafficLightState).map(map)}</TrafficLight>
   );
 };
 
